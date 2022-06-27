d06dd117ac39aac5244d665c95c771c028d17e2c
Felipe Cabral <felipecabral33@gmail.com>
2022-06-27 17:34:32 -0300

    Add Pontuação, Add Tela Vitória, ajuste aúdio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,43 @@
 
-var music = document.getElementById("musicTema");
+let music = document.getElementById("musicTema");
 const BTN_MUTE = document.getElementById("volume-off");
 const BTN_UP = document.getElementById("volume-up");
 
+const BTN_SAVE = document.getElementById("save");
 
+
+
+
+// Tocar a música ao interagir com o corpo do site.
+document.body.addEventListener("mousemove", function () {
+    music.play();
+});
+
+// Função para mutar a música.
 function mute() {
-    music.volume = 0.0;
+    music.muted = true;
     BTN_MUTE.style.display = 'block';
     BTN_UP.style.display = 'none';
 }
+// Função para remover o mute da música.
 function up() {
-    music.volume = 0.5;
+    music.muted = false;
     BTN_MUTE.style.display = 'none';
     BTN_UP.style.display = 'block';
 }
+
+
+let score = Cookies.get('score');
+let player = Cookies.get('player');
+
+if (score == undefined) {
+    score = 9999;
+} else {
+    document.getElementById('score').innerHTML = '<div class="score-players"><h3>' + Cookies.get('player') + '</h3><h3>' + Cookies.get('score') + '</h3></div>';
+}
+
+
+
 
 
 const cards = document.querySelectorAll('.card');
@@ -38,12 +62,24 @@ function flipCard() {
     checkForMatch();
 }
 
+let cont = 0, win = 0;
 //função que checa se as cartas são iguais
 function checkForMatch() {
     if (firstCard.dataset.card === secondCard.dataset.card) {
         disableCards();
+        win++;
+        cont++;
+        document.getElementById("teste").innerHTML = cont;
+        if (win == 9) {
+            document.getElementById("winner").style.animation = "transitionGame 2s ease-in-out forwards";
+            document.getElementById("winner").style.display = "flex";
+            document.getElementById("winner").style.visibility = "visible";
+        }
+
         return;
     }
+    cont++;
+    document.getElementById("teste").innerHTML = cont;
 
     unflipCards();
 }
@@ -77,7 +113,7 @@ function resetBoard() {
 //função que embaralha as cartas
 (function shuffle() {
     cards.forEach((card) => {
-        let ramdomPosition = Math.floor(Math.random() * 12);
+        let ramdomPosition = Math.floor(Math.random() * 18);
         card.style.order = ramdomPosition;
     })
 })();
@@ -86,3 +122,15 @@ function resetBoard() {
 cards.forEach((card) => {
     card.addEventListener('click', flipCard)
 });
+
+BTN_SAVE.addEventListener("click", function () {
+    if (cont <= parseInt(score)) {
+        Cookies.set('player', document.getElementById("nickname").value, { expires: 365 });
+        Cookies.set('score', cont, { expires: 365 });
+        alert("Novo Recorde!!!");
+        document.getElementById('score').innerHTML = '<div class="score-players"><h3>' + Cookies.get('player') + '</h3><h3>' + Cookies.get('score') + '</h3></div>';
+    } else {
+        alert("Sua pontuação ainda não atingiu a melhor, continue tentando!");
+    }
+
+})
